6b67c9ef3753335e091844244151c5ae77543b40
o-oconnell <reamonoshea@gmail.com>
2025-03-04 01:40:21 -0800

    cleanups

--- main.c
+++ main.c
@@ -152,25 +152,25 @@ double length_noPointer_noPointerArgs(vec3 v) {
     return sqrt(length_squared_noPointer_noPointerArgs(v));
 }
 
-vec3 *mul(Arena *arena, double t, vec3 *vec) {
-    vec3 *result = (vec3*) pushArray(arena, sizeof(vec3));
+// vec3 *mul(Arena *arena, double t, vec3 *vec) {
+//     vec3 *result = (vec3*) pushArray(arena, sizeof(vec3));
 
-    result->e[0] = t * vec->e[0];
-    result->e[1] = t * vec->e[1];
-    result->e[2] = t * vec->e[2];
+//     result->e[0] = t * vec->e[0];
+//     result->e[1] = t * vec->e[1];
+//     result->e[2] = t * vec->e[2];
 
-    return result;
-}
+//     return result;
+// }
 
-vec3 mul_nopointer(double t, vec3 *vec) {
-    vec3 result;
+// vec3 mul_nopointer(double t, vec3 *vec) {
+//     vec3 result;
 
-    result.e[0] = t * vec->e[0];
-    result.e[1] = t * vec->e[1];
-    result.e[2] = t * vec->e[2];
+//     result.e[0] = t * vec->e[0];
+//     result.e[1] = t * vec->e[1];
+//     result.e[2] = t * vec->e[2];
 
-    return result;
-}
+//     return result;
+// }
 
 vec3 mul_nopointer_noPointerArgs(double t, vec3 vec) {
     vec3 result;
@@ -265,15 +265,22 @@ vec3 plus_nopointer_noPointerArgs(vec3 a, vec3 b) {
 }
 
 
-vec3 *at(Arena *arena, ray *r, double t) {
-    return plus(arena, &r->origin, mul(arena, t, &r->direction));
+// vec3 *at(Arena *arena, ray *r, double t) {
+//     return plus(arena, &r->origin, mul(arena, t, &r->direction));
+// }
+
+// vec3 at_nopointer(ray *r, double t) {
+//     vec3 mulResult = mul_nopointer(t, &r->direction);
+
+//     return plus_nopointer(&r->origin, &mulResult);
+// }
+
+vec3 at_nopointer_noPointerArgs(ray r, double t) {
+    vec3 mulResult = mul_nopointer_noPointerArgs(t, r.direction);
+
+    return plus_nopointer_noPointerArgs(r.origin, mulResult);
 }
 
-vec3 at_nopointer(ray *r, double t) {
-    vec3 mulResult = mul_nopointer(t, &r->direction);
-
-    return plus_nopointer(&r->origin, &mulResult);
-}
 
 vec3 *plus_vararg(Arena *arena, vec3 *first, ...) {
     vec3 *result = (vec3*) pushArray(arena, sizeof(vec3));
@@ -414,16 +421,16 @@ vec3 minus_nopointer_noPointerArgs(vec3 a, vec3 b) {
 
 
 
-double x(vec3 *v) {
-    return v->e[0];
+double x(vec3 v) {
+    return v.e[0];
 }
 
-double y(vec3 *v) {
-    return v->e[1];
+double y(vec3 v) {
+    return v.e[1];
 }
 
-double z(vec3 *v) {
-    return v->e[2];
+double z(vec3 v) {
+    return v.e[2];
 }
 
 void write_color(vec3 pixel_color) {
@@ -437,8 +444,6 @@ void write_color(vec3 pixel_color) {
 
     printf("%d %d %d\n", rbyte, gbyte, bbyte);
 }
-
-
 
 char *vec2str(Arena *arena, vec3 *vec) {
     char *result = pushArray(arena, 100*sizeof(char));
@@ -457,16 +462,11 @@ char *vec2str_noArena(vec3 vec) {
 
 
 double dot(vec3 u, vec3 v) {
-
     return u.e[0] * v.e[0] 
             + u.e[1] * v.e[1]
             + u.e[2] * v.e[2];
 }
 
-
-int hit_record_num = 0;
-
-int hit_record_max = 2000;
 
 double hit_sphere_alt(vec3 center, double radius, ray r, double ray_tmin, double ray_tmax, hit_record *rec) {
     double a = dot(r.direction, r.direction);
@@ -486,9 +486,7 @@ double hit_sphere_alt(vec3 center, double radius, ray r, double ray_tmin, double
     }
 
     rec->t = root;
-    vec3 atval = at_nopointer(&r, rec->t);
-
-    hit_record_num++;
+    vec3 atval = at_nopointer_noPointerArgs(r, rec->t);
 
     rec->p.e[0] = atval.e[0];
     rec->p.e[1] = atval.e[1];
@@ -515,7 +513,7 @@ double hit_sphere_alt(vec3 center, double radius, ray r, double ray_tmin, double
 }
 
 #define MAX_DIFFUSE_DEPTH 50
-#define HITTABLES_LENGTH 500
+#define HITTABLES_LENGTH 4
 
 hittable hittables[HITTABLES_LENGTH];
 
@@ -553,29 +551,17 @@ double random_double_range(double min, double max) {
     return min + (max - min)*random_double();
 }
 
-vec3 *random_vec3(Arena *arena) {
-    return newVec3(arena, random_double(), random_double(), random_double());
-} 
-
-vec3 random_vec3_noPointer() {
+vec3 random_vec3() {
     return newVec3_noArena(random_double(), random_double(), random_double());
-} 
-
-vec3 *random_range_vec3(Arena *arena, double min, double max) {
-    return newVec3(arena, random_double_range(min,max), random_double_range(min,max), random_double_range(min,max));
 }
 
-vec3 random_range_vec3_noPointer(double min, double max) {
+vec3 random_range_vec3(double min, double max) {
     return newVec3_noArena(random_double_range(min,max), random_double_range(min,max), random_double_range(min,max));
 }
 
-// vec3 random_range_vec3_noPointer(double min, double max) {
-//     return newVec3_noArena(random_double_range(min,max), random_double_range(min,max), random_double_range(min,max));
-// }
-
-vec3 random_unit_vector_noArena() {
+vec3 random_unit_vector() {
     while (true) {
-        vec3 p = random_range_vec3_noPointer( -1.0, 1.0);
+        vec3 p = random_range_vec3( -1.0, 1.0);
         double lensq = length_squared_noPointer_noPointerArgs(p);
         
         if (1e-160 <= lensq <= 1) {
@@ -584,32 +570,18 @@ vec3 random_unit_vector_noArena() {
     }
 }
 
-vec3 *random_unit_vector(Arena *arena) {
-    while (true) {
-        vec3* p = random_range_vec3(arena, -1.0, 1.0);
-        double lensq = length_squared(arena, p);
-        
-        if (1e-160 <= lensq <= 1) {
-            return vec_div(arena, p, sqrt(lensq));
-        }
-    }
-}
-
 vec3 reflect_new(vec3 incoming, vec3 n) {
-
     vec3 L = mul_nopointer_noPointerArgs( -1.0, incoming);
-
     double NL = 2 * dot(n, L);
 
     vec3 comp_a = mul_nopointer_noPointerArgs(NL, n);
-
     return minus_nopointer_noPointerArgs(comp_a, L);
 }
 
 vec3 refract(vec3 uv, vec3 n, double etai_over_etat) {
-
     double uvn = dot(mul_nopointer_noPointerArgs(-1.0, uv), n);
     double cos_theta = uvn;
+
     if (cos_theta > 1.0) {
         cos_theta = 1.0;
     }
@@ -623,7 +595,6 @@ vec3 refract(vec3 uv, vec3 n, double etai_over_etat) {
 }
 
 vec3 vec_mul_noPointer(vec3 a, vec3 b) {
-
     return newVec3_noArena(a.e[0]*b.e[0], a.e[1]*b.e[1], a.e[2]*b.e[2]);
 }
 
@@ -641,12 +612,7 @@ vec3 ray_color(ray r, int depth) {
     hit_record rec;
     if (worldHit(r, 0.001, DBL_MAX, &rec)) {
         if (rec.material == LAMBERTIAN) {
-            vec3 random_uv = random_unit_vector_noArena();
-
-            // QUESTION: WHY DOES IT MAKE ALL BLACK IF THIS IS A MINUS? 
-            // ^TODO: ANSWER THIS QUESTION SO YOU CAN UNDERSTAND YOUR SHIT RETARD
-            // ^From plotting a couple points: SEEMS to be the case that minus results in a vector
-            // that's always inside the sphere. So always intersects and gives us black.
+            vec3 random_uv = random_unit_vector();
             vec3 direction = plus_nopointer_noPointerArgs(random_uv, rec.normal);
             vec3 minus_direction = plus_nopointer_noPointerArgs(random_uv, rec.normal);
 
@@ -654,7 +620,7 @@ vec3 ray_color(ray r, int depth) {
                 direction = rec.normal;
             }
             
-            double direction_magnitude = sqrt(x(&direction) * x(&direction) + y(&direction) * y(&direction) + z(&direction) * z(&direction));
+            double direction_magnitude = sqrt(x(direction) * x(direction) + y(direction) * y(direction) + z(direction) * z(direction));
 
             ray ray_new = newRay_noArena(rec.p, direction);
             vec3 result = vec_mul_noPointer(rec.albedo, ray_color(ray_new, depth -1)); 
@@ -689,7 +655,7 @@ vec3 ray_color(ray r, int depth) {
     }
 
     vec3 unit_direction = unit_vector_noPointer(r.direction);
-    double a = 0.5 * (y(&unit_direction) + 1.0);
+    double a = 0.5 * (y(unit_direction) + 1.0);
     
     // makes the blue background. lerp.
     vec3 result =  plus_nopointer_noPointerArgs(mul_nopointer_noPointerArgs((1.0 - a), newVec3_noArena(1.0, 1.0, 1.0)), mul_nopointer_noPointerArgs(a, newVec3_noArena(0.5, 0.7, 1.0)));
@@ -703,19 +669,11 @@ double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
-
-vec3 *cross(Arena *arena, vec3 *u, vec3 *v) {
-    return newVec3(arena, u->e[1] * v->e[2] - u->e[2] * v->e[1],
-                          u->e[2] * v->e[0] - u->e[0] * v->e[2],
-                          u->e[0] * v->e[1] - u->e[1] * v->e[0]);
-}
-
-vec3 cross_noPointer(vec3 u, vec3 v) {
+vec3 cross(vec3 u, vec3 v) {
     return newVec3_noArena(u.e[1] * v.e[2] - u.e[2] * v.e[1],
                           u.e[2] * v.e[0] - u.e[0] * v.e[2],
                           u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
-
 
 int main() {
     srand(time(NULL));
@@ -723,10 +681,6 @@ int main() {
     size_t arena_size = (1 << 30); // 1 GB
     Arena *arena = arenaAlloc(arena_size);
 
-    // vec3 *center2 = newVec3(arena, 0, -100.5, -1);
-    // vecCopy(arena, &hittables[0].center, center2);
-
-    // turns black if you set to -1000 - makes sense right? You're inside the sphere. 
     hittables[0].center = newVec3_noArena(0, -1000, 0);
     hittables[0].radius = 1000;
     hittables[0].type = SPHERE;
@@ -735,37 +689,37 @@ int main() {
 
     int hittables_index = 1;
 
-    for (int a = -11; a < 11; ++a) {
-        for (int b = -11; b < 11; ++b) {
-            double choose_mat = random_double();
+    // for (int a = -11; a < 11; ++a) {
+    //     for (int b = -11; b < 11; ++b) {
+    //         double choose_mat = random_double();
 
-            vec3 center = newVec3_noArena(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+    //         vec3 center = newVec3_noArena(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
 
-            if (length_noPointer_noPointerArgs(minus_nopointer_noPointerArgs(center, newVec3_noArena(4, 0.2, 0))) > 0.9) {
-                hittables[hittables_index].center = center;
-                hittables[hittables_index].radius = 0.2;
+    //         if (length_noPointer_noPointerArgs(minus_nopointer_noPointerArgs(center, newVec3_noArena(4, 0.2, 0))) > 0.9) {
+    //             hittables[hittables_index].center = center;
+    //             hittables[hittables_index].radius = 0.2;
 
-                if (choose_mat < 0.8) {
-                    vec3 albedo = vec_mul_noPointer(random_vec3_noPointer(), random_vec3_noPointer());
+    //             if (choose_mat < 0.8) {
+    //                 vec3 albedo = vec_mul_noPointer(random_vec3(), random_vec3());
 
-                    hittables[hittables_index].material = LAMBERTIAN;
-                    hittables[hittables_index].albedo = albedo;
-                } else if (choose_mat < 0.95) {
-                    vec3 albedo = random_range_vec3_noPointer(0.5, 1);
+    //                 hittables[hittables_index].material = LAMBERTIAN;
+    //                 hittables[hittables_index].albedo = albedo;
+    //             } else if (choose_mat < 0.95) {
+    //                 vec3 albedo = random_range_vec3(0.5, 1);
                     
-                    hittables[hittables_index].material = METAL;
-                    hittables[hittables_index].albedo = albedo;
-                } else {
+    //                 hittables[hittables_index].material = METAL;
+    //                 hittables[hittables_index].albedo = albedo;
+    //             } else {
 
-                    hittables[hittables_index].refraction_index = 1.5;
-                    hittables[hittables_index].material = DIELECTRIC;
-                    hittables[hittables_index].albedo = newVec3_noArena(0.7, 0.6, 0.5);
-                }
+    //                 hittables[hittables_index].refraction_index = 1.5;
+    //                 hittables[hittables_index].material = DIELECTRIC;
+    //                 hittables[hittables_index].albedo = newVec3_noArena(0.7, 0.6, 0.5);
+    //             }
 
-                hittables_index++;
-            }
-        }
-    }
+    //             hittables_index++;
+    //         }
+    //     }
+    // }
 
 
     hittables[hittables_index].center = newVec3_noArena(0, 1, 0);
@@ -846,90 +800,41 @@ int main() {
     // }
 
     double aspect_ratio = 16.0/9.0; 
-    int image_width = 1200;
+    int image_width = 400;
 
     int image_height = (int)(image_width / aspect_ratio); 
 
     vec3 lookfrom = newVec3_noArena(13,2,3); // why does negative Z values go further out? 
     vec3 lookat = newVec3_noArena(0, 0, 0);
     vec3 vup = newVec3_noArena(0, 1, 0);
-
-
-    // vec3 *camera_center = newVec3(arena, 0, 0, 0);
     vec3 camera_center = lookfrom;
 
     double focal_length = length_noPointer_noPointerArgs(minus_nopointer_noPointerArgs(lookfrom, lookat));
-
-    // double focal_length = 1.0;
-    // double viewport_height = 2.0;
-
-    // not sure why this is actually the FOV rather than relating to the height?
-    // but it does definitely increase the FOV!
     double vfov = 90;
     double theta = degrees_to_radians(vfov);
-
-    // We have depth of 1 always 
-    // so the adjacent has magnitude 1
-    // The corner angle of the triangle is theta/2
-    // since theta = 90 is the full angle of the camera
-    // tan(theta/2) = opposite / adj
-    // adj * tan(theta/2) = opposite
-    // adj * tan(theta/2) = h
-    // Adj = 1, so tan(theta/2) = h
     double h = tan(theta/2);
-
-    // I guess each time we go further inwards in the Z direction the focal length increases,
-    // hence multiply by focal length? Otherwise only makes sense to be 2*h.
     double viewport_height = 2 * h * focal_length;
-
     double viewport_width = viewport_height * (((double)image_width) / image_height);
+    
+    fprintf(stderr, "Rendering...\n");
     fprintf(stderr, "Viewport height is %lf\n", viewport_height);
     fprintf(stderr, "Viewport width is %lf\n", viewport_width);
 
     vec3 w = unit_vector_noPointer(minus_nopointer_noPointerArgs(lookfrom, lookat));
-
-    // right hand rule with vup and pointing towards the z direction
-    // gives a vector that points to the right
-    vec3 u = unit_vector_noPointer(cross_noPointer(vup, w));
-
-    // right hand rule with the vector pointing in the z direction and 
-    // the vector pointing right gives a vector that is similar to vup 
-    // but instead of pointing straight up points up "in the plane" normal to 
-    // the camera direction
-    vec3 v = cross_noPointer(w, u);
-
+    vec3 u = unit_vector_noPointer(cross(vup, w));
+    vec3 v = cross(w, u);
     vec3 viewport_u = mul_nopointer_noPointerArgs(viewport_width, u);
-    
-    // why multiply by -1? 
     vec3 viewport_v = mul_nopointer_noPointerArgs(viewport_height, mul_nopointer_noPointerArgs(-1.0, v)); 
-
-
-    // vec3 *viewport_u = newVec3(arena, viewport_width, 0, 0);
-    // vec3 *viewport_v = newVec3(arena, 0, -viewport_height, 0);
-    
     vec3 pixel_delta_u = vec_div_noPointer_noPointerArgs(viewport_u, image_width);
     vec3 pixel_delta_v = vec_div_noPointer_noPointerArgs(viewport_v, image_height);
-    
-    fprintf(stderr, "Pixel delta u is %lf\n", x(&pixel_delta_u));
-    fprintf(stderr, "Pixel delta v is %lf\n", y(&pixel_delta_v));
-
-
-    // fprintf(stderr, "viewport_u/2 IS %s\n", vec2str(arena, minus(arena, camera_center, vec_div(arena, viewport_u, 2.0))));
-
-
-    // mul(arena, focal_length, w) - multiplies unit vector pointing in the Z direction by the actual length
-    // viewport_u / 2 = half of the width
-    // viewport_v / 2 = half of the height
-
     vec3 viewport_upper_left = minus_vararg_nopointer(4, camera_center, mul_nopointer_noPointerArgs(focal_length, w), vec_div_noPointer_noPointerArgs(viewport_u, 2), vec_div_noPointer_noPointerArgs(viewport_v, 2));
-
-    // vec3 *viewport_upper_left = minus_vararg(arena, camera_center, newVec3(arena, 0, 0, focal_length), vec_div(arena, viewport_u, 2), vec_div(arena, viewport_v, 2), NULL);
-
+    
     vec3 arg2 = mul_nopointer_noPointerArgs(0.5, plus_nopointer_noPointerArgs(pixel_delta_u, pixel_delta_v));
     vec3 pixel00_loc = plus_nopointer_noPointerArgs(viewport_upper_left, arg2);
-    fprintf(stderr, "Viewport upper left is %s\n", vec2str(arena, &viewport_upper_left));
 
-    fprintf(stderr, "Pixel 0,0 location is %s\n", vec2str(arena, &pixel00_loc));
+    // fprintf(stderr, "Viewport upper left is %s\n", vec2str(arena, &viewport_upper_left));
+    // fprintf(stderr, "Pixel 0,0 location is %s\n", vec2str(arena, &pixel00_loc));
+
     printf("P3\n%d %d\n255\n", image_width, image_height);
 
     int samples_per_pixel = 10;
@@ -942,20 +847,11 @@ int main() {
             int arg_count = 3;
             vec3 arg2 = mul_nopointer_noPointerArgs(i, pixel_delta_u); 
             vec3 arg3 = mul_nopointer_noPointerArgs(j, pixel_delta_v);
-
             vec3 pixel_center = plus_vararg_nopointer(arg_count, pixel00_loc, arg2, arg3);
-
-            if (hit_record_num < hit_record_max) {
-                // fprintf(stderr, "Pixel center is %s (j = %d, i = %d)\n", vec2str(arena, pixel_center), j, i);
-            }
 
             for (int sample = 0; sample < samples_per_pixel; sample++) {
                 vec3 ray_direction = minus_nopointer_noPointerArgs(pixel_center, camera_center);
                 ray r = newRay_noArena(camera_center, ray_direction);
-
-                if (hit_record_num < hit_record_max) {
-                    // fprintf(stderr, "IN MAIN Origin is %s\n", vec2str(arena, &r->origin));
-                }
 
                 pixel_color = plus_nopointer_noPointerArgs(pixel_color, ray_color(r, MAX_DIFFUSE_DEPTH));
             }
